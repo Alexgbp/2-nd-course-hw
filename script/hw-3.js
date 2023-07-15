01ae120799f1d6bab99a3151802f661c009c65cd
Alexgbp <bboy91@bk.ru>
2023-07-15 09:54:54 +0300

    Выполнено задание HW-4

--- script/hw-3.js
+++ script/hw-3.js
@@ -1,151 +1,151 @@
-//Задание 1
+// //Задание 1
 
-let password = "пароль";
-let userAnswer = String(prompt("Введите пароль"));
-userAnswer = userAnswer.toLocaleLowerCase();
+// let password = "пароль";
+// let userAnswer = String(prompt("Введите пароль"));
+// userAnswer = userAnswer.toLocaleLowerCase();
 
-if(userAnswer === password){
-    console.log("Пароль введен верно");
-}else{
-    console.log("Пароль введен неправильно");
-}
+// if(userAnswer === password){
+//     console.log("Пароль введен верно");
+// }else{
+//     console.log("Пароль введен неправильно");
+// }
 
-// // Задание 2
+// // // Задание 2
 
-let c = 0;
+// let c = 0;
 
-if(c > 0 && c < 10){
-    console.log("Верно");
-}else{
-    console.log("Не верно");
-}
- c = 10;
+// if(c > 0 && c < 10){
+//     console.log("Верно");
+// }else{
+//     console.log("Не верно");
+// }
+//  c = 10;
 
-if(c > 0 && c < 10){
-    console.log("Верно");
-}else{
-    console.log("Не верно");
-}
-c = -3;
+// if(c > 0 && c < 10){
+//     console.log("Верно");
+// }else{
+//     console.log("Не верно");
+// }
+// c = -3;
 
-if(c > 0 && c < 10){
-    console.log("Верно");
-}else{
-    console.log("Не верно");
-}
-c = 2;
+// if(c > 0 && c < 10){
+//     console.log("Верно");
+// }else{
+//     console.log("Не верно");
+// }
+// c = 2;
 
-if(c > 0 && c < 10){
-    console.log("Верно");
-}else{
-    console.log("Не верно");
-}
-
-
-// // Условие "Верно" отрабатывает только в 4 случае
+// if(c > 0 && c < 10){
+//     console.log("Верно");
+// }else{
+//     console.log("Не верно");
+// }
 
 
-
-// Задание 3
-
-
-let d = 10;
-let e = 102;
-
-if(d > 100 || e > 100){
-    console.log("Верно");
-}else{
-    console.log("Не верно");
-}
+// // // Условие "Верно" отрабатывает только в 4 случае
 
 
-// Задание 4
 
-let a = '2';
-let b = '3';
-alert (parseInt(a) + parseInt(b));
-alert (Number(a) + Number(b));
+// // Задание 3
 
 
-// Задание 5
+// let d = 10;
+// let e = 102;
 
-let monthNumber = Number(prompt("Введите номер месяца от 1 до 12"))
+// if(d > 100 || e > 100){
+//     console.log("Верно");
+// }else{
+//     console.log("Не верно");
+// }
+
+
+// // Задание 4
+
+// let a = '2';
+// let b = '3';
+// alert (parseInt(a) + parseInt(b));
+// alert (Number(a) + Number(b));
+
+
+// // Задание 5
+
+// let monthNumber = Number(prompt("Введите номер месяца от 1 до 12"))
  
 
-switch (monthNumber) {
-    case 1:
-        console.log("1-й месяц принадлежит к сезону «зима»");
-        break;
-    case 2:
-        console.log("2-й месяц принадлежит к сезону «зима»");
-        break;
-     case 3:
-        console.log("3-й месяц принадлежит к сезону «весна»");
-        break;
-    case 4:
-        console.log("4-й месяц принадлежит к сезону «весна»");
-        break;
-    case 5:
-        console.log("5-й месяц принадлежит к сезону «весна»");
-        break;
-    case 6:
-        console.log("6-й месяц принадлежит к сезону «лето»");
-        break;
-    case 7:
-        console.log("7-й месяц принадлежит к сезону «лето»");
-        break;
-     case 8:
-        console.log("8-й месяц принадлежит к сезону «лето»");
-        break;
-    case 9:
-        console.log("9-й месяц принадлежит к сезону «осень»");
-        break;
-    case 10:
-        console.log("10-й месяц принадлежит к сезону «осень»");
-        break;
-    case 11:
-        console.log("11-й месяц принадлежит к сезону «осень»");
-        break;
-    case 12:
-        console.log("12-й месяц принадлежит к сезону «зима»");
-        break;
-    default:
-            console.log("Такого месяца нет");
-        break;
-}
+// switch (monthNumber) {
+//     case 1:
+//         console.log("1-й месяц принадлежит к сезону «зима»");
+//         break;
+//     case 2:
+//         console.log("2-й месяц принадлежит к сезону «зима»");
+//         break;
+//      case 3:
+//         console.log("3-й месяц принадлежит к сезону «весна»");
+//         break;
+//     case 4:
+//         console.log("4-й месяц принадлежит к сезону «весна»");
+//         break;
+//     case 5:
+//         console.log("5-й месяц принадлежит к сезону «весна»");
+//         break;
+//     case 6:
+//         console.log("6-й месяц принадлежит к сезону «лето»");
+//         break;
+//     case 7:
+//         console.log("7-й месяц принадлежит к сезону «лето»");
+//         break;
+//      case 8:
+//         console.log("8-й месяц принадлежит к сезону «лето»");
+//         break;
+//     case 9:
+//         console.log("9-й месяц принадлежит к сезону «осень»");
+//         break;
+//     case 10:
+//         console.log("10-й месяц принадлежит к сезону «осень»");
+//         break;
+//     case 11:
+//         console.log("11-й месяц принадлежит к сезону «осень»");
+//         break;
+//     case 12:
+//         console.log("12-й месяц принадлежит к сезону «зима»");
+//         break;
+//     default:
+//             console.log("Такого месяца нет");
+//         break;
+// }
 
  
 
-// Задание 7
+// // Задание 7
 
-let insertNumber = Number(prompt("Пожалуйста, введите любое число"));
-console.log(isNaN(insertNumber));
+// let insertNumber = Number(prompt("Пожалуйста, введите любое число"));
+// console.log(isNaN(insertNumber));
 
-if( insertNumber % 2 === 0){
-    console.log("Число четное")
-}else{
-    console.log("Число  нечетное")
-}
+// if( insertNumber % 2 === 0){
+//     console.log("Число четное")
+// }else{
+//     console.log("Число  нечетное")
+// }
 
 
-// Задание 8 и Задание 9 обьединил, т.к такой вариант кажется логичнее 
+// // Задание 8 и Задание 9 обьединил, т.к такой вариант кажется логичнее 
 
-const clientDeviceYear = 2015;
-let clientOS = Number(prompt("Какая ситема на вашем телефоне: Android (введите 1), IOS (введите 0)"));
-let clientAnswer  = Number(prompt("Укажите год вашего телефона?"))
+// const clientDeviceYear = 2015;
+// let clientOS = Number(prompt("Какая ситема на вашем телефоне: Android (введите 1), IOS (введите 0)"));
+// let clientAnswer  = Number(prompt("Укажите год вашего телефона?"))
 
-if( clientOS === 0) {
-    if (clientAnswer < clientDeviceYear){
-        console.log("Установите облегченную версию приложения для iOS по ссылке");
-    }else{
-        console.log("Установите версию приложения для iOS по ссылке");
-    }
-} else if(clientOS === 1){
-    if(clientAnswer < clientDeviceYear){
-        console.log("Установите облегченную версию приложения для Android по ссылке");
-    }else{
-        console.log("Установите версию приложения для Android по ссылке");
-    }
-}
+// if( clientOS === 0) {
+//     if (clientAnswer < clientDeviceYear){
+//         console.log("Установите облегченную версию приложения для iOS по ссылке");
+//     }else{
+//         console.log("Установите версию приложения для iOS по ссылке");
+//     }
+// } else if(clientOS === 1){
+//     if(clientAnswer < clientDeviceYear){
+//         console.log("Установите облегченную версию приложения для Android по ссылке");
+//     }else{
+//         console.log("Установите версию приложения для Android по ссылке");
+//     }
+// }
 
 
---
+++ script/hw-4.js
@@ -0,0 +1,64 @@
+// Задание 1
+
+let i = 0;
+
+while(i < 2){
+    i++
+    console.log("Привет");
+}
+
+
+// Задание 2
+
+let i1 = 0;
+
+while(i1 < 5){
+    i1++
+    console.log(i1);
+}
+
+// Задание 3
+
+let i2 = 6;
+
+while(i2 < 22){
+    i2++
+    console.log(i2);
+}
+
+
+// Задание 4
+
+let obj = {
+    "Коля": 200,
+    "Вася": 300,
+    "Петя": 400,
+}
+for( let key in obj){
+    console.log(`${key} зарплата ${obj[key]} долларов`);
+}
+
+
+
+// Задание 5
+
+let num = 0;
+let n = 1000;
+while(n >= 50){
+    n/=2
+    num++
+    console.log(n);
+}
+console.log(`Колличество иттераций: ${num}`);
+
+
+
+
+// Задание 6
+
+let friday = 5;
+
+for(let i = friday; i <=31; i+=7){
+    console.log(`Сегодня пятница ${i}-е число.Необходимо подготовить отчет.`);
+}
+
