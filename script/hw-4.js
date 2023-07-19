390bef87a755d0872ba4b403739dc4ea77615d2e
Alexgbp <bboy91@bk.ru>
2023-07-19 08:59:29 +0300

    Выполнено HW-5

--- script/hw-4.js
+++ script/hw-4.js
@@ -1,64 +1,64 @@
-// Задание 1
+// // Задание 1
 
-let i = 0;
+// let i = 0;
 
-while(i < 2){
-    i++
-    console.log("Привет");
-}
-
-
-// Задание 2
-
-let i1 = 0;
-
-while(i1 < 5){
-    i1++
-    console.log(i1);
-}
-
-// Задание 3
-
-let i2 = 6;
-
-while(i2 < 22){
-    i2++
-    console.log(i2);
-}
+// while(i < 2){
+//     i++
+//     console.log("Привет");
+// }
 
 
-// Задание 4
+// // Задание 2
 
-let obj = {
-    "Коля": 200,
-    "Вася": 300,
-    "Петя": 400,
-}
-for( let key in obj){
-    console.log(`${key} зарплата ${obj[key]} долларов`);
-}
+// let i1 = 0;
+
+// while(i1 < 5){
+//     i1++
+//     console.log(i1);
+// }
+
+// // Задание 3
+
+// let i2 = 6;
+
+// while(i2 < 22){
+//     i2++
+//     console.log(i2);
+// }
 
 
+// // Задание 4
 
-// Задание 5
-
-let num = 0;
-let n = 1000;
-while(n >= 50){
-    n/=2
-    num++
-    console.log(n);
-}
-console.log(`Колличество иттераций: ${num}`);
+// let obj = {
+//     "Коля": 200,
+//     "Вася": 300,
+//     "Петя": 400,
+// }
+// for( let key in obj){
+//     console.log(`${key} зарплата ${obj[key]} долларов`);
+// }
 
 
 
+// // Задание 5
 
-// Задание 6
+// let num = 0;
+// let n = 1000;
+// while(n >= 50){
+//     n/=2
+//     num++
+//     console.log(n);
+// }
+// console.log(`Колличество иттераций: ${num}`);
 
-let friday = 5;
 
-for(let i = friday; i <=31; i+=7){
-    console.log(`Сегодня пятница ${i}-е число.Необходимо подготовить отчет.`);
-}
+
+
+// // Задание 6
+
+// let friday = 5;
+
+// for(let i = friday; i <=31; i+=7){
+//     console.log(`Сегодня пятница ${i}-е число.Необходимо подготовить отчет.`);
+// }
 
---
+++ script/script.js
@@ -0,0 +1,10 @@
+let startGame = () =>{
+
+    let start = confirm("Хотите запустить игру ?")
+
+    if(start){
+        alert("Игра началась");
+    }else{
+        alert("Жаль. До встречи!");
+    }
+}
