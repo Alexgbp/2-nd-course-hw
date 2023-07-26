8280718e3a08cb2989550c5cd28e4525c1434f8d
Alexgbp <bboy91@bk.ru>
2023-07-26 16:23:49 +0300

    ДЗ hw-7

--- script/script.js
+++ script/script.js
@@ -16,3 +16,31 @@ let getMonths  = () => {
         console.log("Осень");
     }
 }
+
+
+
+// Задание 11
+
+
+ let array = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+ let result = array.sort(() => Math.random() - 0.5);
+
+
+function rememberWords(){
+
+    alert(`Посмотрите на список фруктов, запомните порядок и нажмите "Ok" чтобы продолжить: ${result}`);
+
+    let firstQuest = prompt("Введите название первого фрукта из списка?");
+    let secondQuest = prompt("Введите название последнего фрукта из списка?");
+    
+    if (!firstQuest || !secondQuest) {
+        alert("Вы ничего не ввели");
+      } else if (firstQuest.toLowerCase() === result[0].toLowerCase() && secondQuest.toLowerCase() === result[result.length - 1].toLowerCase()) {
+        alert("Поздравляю, Вы угадали!");
+      } else if (firstQuest.toLowerCase() === result[0].toLowerCase() || secondQuest.toLowerCase() === result[result.length - 1].toLowerCase()) {
+        alert("Вы были близки к победе!");
+      } else {
+        alert("Вы ответили не верно");
+      }
+    }
+
